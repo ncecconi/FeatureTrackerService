68e94eaa1138a566c9ab8957d65008d3ce3d8e75
ncecconi <nick.cecconi@gmail.com>
2017-05-16 07:28:07 -0500

    Updated release web.config - TODO replace with azure connection string

---
+++ FeatureTrackerService/Migrations/201705050239332_initial_home.cs
@@ -0,0 +1,16 @@
+namespace FeatureTrackerService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial_home : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
